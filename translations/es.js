0952becac6fd3b5ed3329695d6d9d2e2e6679bf6
Isaac Yriarte <isaacyriarte@gmail.com>
2017-11-03 20:39:41 -0400

    Se agregan los i18n y se enlazan en toda la pagina.
    Tambien se mueven la pantalla modal para el final de la pagina.

--- translations/es.js
+++ translations/es.js
@@ -1,5 +1,74 @@
 const i18nEs = {
-  nav: {
-    home: 'Inicio'
+  "nav": {
+    "home": "Inicio",
+    "services": "Servicios",
+    "works": "Proyectos",
+    "team": "Sobre mí",
+    "testimonials": "Opiniones",
+    "contact": "Contacto"
+  },
+  "banner": {
+    "banner_subtitle": "Programador Web/ App Developer"
+  },
+  "intro": {
+    "title": "¡Puedo ayudarte con tu Proyecto!",
+    "description": "Soy una persona honesta, proactiva, responsable, comprometida y con disposición para la búsqueda continua de información a fin de ofrecer un servicio eficiente y soluciones de calidad a mis clientes.<br> Tengo disponibilidad para trabajar de manera presencial o remota."
+  },
+  "services": {
+    "consultAndAnalysis_title": "Consultoría & Análisis",
+    "consultAndAnalysis_description": "Análisis de los requerimientos  y necesidades del cliente en la creación de su proyecto.",
+    "designWeb_title": "Diseño y Desarrollo Web",
+    "designWeb_description": "Diseño y Desarrollo Web, utilizando lenguajes de programación y herramientas computacionales adecuadas.",
+    "designMobile_title": "Diseño y Desarrollo Web",
+    "designMobile_description": "Diseño y Desarrollo Web, utilizando lenguajes de programación y herramientas computacionales adecuadas.",
+    "control_title": "Control y Seguimiento",
+    "control_description": "Controles continuos del proceso y del producto, a fin de asegurar la satisfacción del cliente y el cumplimiento de los estándares de calidad.",
+    "maintenance_title": "Mantenimiento y Actualización de Páginas Web",
+    "maintenance_description": "Actualización de contenido, cambio de diseño y mantenimiento de páginas web ya existentes.",
+    "cvs_title": "Control de versiones",
+    "cvs_description": "Utilizando Sistemas de Control de Versiones como Git o SVN."
+  },
+  "works": {
+    "work1_title": "Pizzeria-Spicchi Di Sole",
+    "work1_description": "Diseño de Página Web",
+    "work2_title": "Photographer Freelance",
+    "work2_description": "Portafolio Fotográfico",
+    "work3_title": "SantAngelo Frutta e Verdura",
+    "work3_description": "Diseño de Página Web",
+    "work4_title": "Freelance Translator",
+    "work4_description": "Portafolio Personal",
+  },
+  "teams": {
+    "title": "Isaac Yriarte",
+    "subtitle": "Ingeniero en Informática",
+    "description": "Actualmente formo parte de una empresa donde me desempeño desarrollando aplicaciones web y móviles.<br/>  También desarrollo proyectos individuales como profesional freelance.<br/>Los lenguajes de programación que más utilizo actualmente son: Java, C#, PHP, PHP OO, JavaScript, Python."
+  },
+  "testimonials": {
+    "testimonial1": "\"Isaac es un desarrollador web talentoso y hábil, altamente comprometido con su trabajo, excelente profesional y con mucha calidad humana.\"",
+    "author1": "Aracelis Martín, Freelance RA Consultant",
+    "testimonial2": "\"Isaac is a highly responsible person, with a high academic and professional level, strongly recommended.\"",
+    "author2": "Giorgio Iule, Freelance Photographer.",
+    "testimonial3": "\"Isaac è un professionista preoccupato per il suo costante aggiornamento, orientato al raggiungimento degli obiettivi, dedicato, impegnato e appassionato del suo lavoro, altamente responsabile. Consegna i progetti nel tempo concordato.\"",
+    "author3": "Maurizio Buonpensiero, Commissionaria SantAngelo",
+    "testimonial4": "\"Recomiendo a Isaac para el trabajo individual, pero también para proyectos compartidos; por su disposición a colaborar con los miembros del equipo y su proactividad para contribuir con el logro de los objetivos.\"",
+    "author4": "Alejandra Yriarte, Corporación A&A"
+  },
+  "contact": {
+    "title": "Comunícate conmigo",
+    "subtitle": "Estoy a la orden para responder tus dudas y orientarte de manera gratuita sobre el proyecto que deseas realizar.",
+    "form": {
+      "name_placeholder": "Nombre",
+      "email_placeholder": "Correo",
+      "message_placeholder": "Mensaje",
+      "button_send": "Enviar"
+    }
+  },
+  "footer": {
+    "title": "Contacto",
+    //"credits": "Iconoos diseñados por <a href='http://www.freepik.com' title='Freepik'>Freepik</a> desde <a href='https://www.flaticon.es/' title='Flaticon'>www.flaticon.com</a> con licencia <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'"
+  },
+  "modal": {
+    "title": "Idioma",
+    "button_save": "Guardar"
   }
 };
