14da98bbbbac9cdfe93750bf70a01e4c11c46876
alelisa <alelisa@git.githubdemo>
2017-11-06 13:56:09 +0100

    Adding italian translation

---
+++ translations/it.js
@@ -0,0 +1,80 @@
+const i18nEs = {
+  "nav": {
+    "home": "Home",
+    "services": "Servizi",
+    "works": "Proggtti",
+    "team": "Su di me",
+    "testimonials": "Testimonianze",
+    "contact": "Contatto"
+  },
+  "banner": {
+    "title": "Isaac Yriarte",
+    "subtitle": "Sviluppatore Web/ App Developer"
+  },
+  "intro": {
+    "title": "¡Posso aiutarti con il tuo progetto!",
+    "description": "
+Sono una persona onesta, proattiva, responsabile,  nella continua ricerca di aggiornamenti di sistema e nuova informazione con lo scoppo  offrire un servizio efficiente e soluzioni di qualità per i miei clienti.
+Sono disponibile lavorare in persona o di forma remota"
+  },
+  "services": {
+    "consultAndAnalysis_title": "CONSULENZA E ANALISI",
+    "consultAndAnalysis_description": "Analisi dei requisiti ed  esigenze del cliente nella creazione del suo progetto.",
+    "designWeb_title": "DESIGN E SVILUPPO WEB",
+    "designWeb_description": "Web Design e Sviluppo web, utilizzando linguaggi di programmazione e strumenti appropriati.",
+    "designMobile_title": "PROGETTAZIONE E SVILUPPO DI APPLICAZIONI MOBILI",
+    "designMobile_description": "Progettazione e sviluppo di applicazioni mobili per Android di forma nativa e multipiattaforma utilizzando Kony Studio.",
+    "control_title": "Controllo e Monitoraggio",
+    "control_description": "Controlli continui del processo e dei prodotti, al fine di garantire la soddisfazione del cliente e la conformità con gli standard di qualità.",
+    "maintenance_title": "Manutenzione e aggiornamento di siti Web",
+    "maintenance_description": "Aggiornamento del contenuto, del design e continua evoluzione tecnica per un sito sempre all'avanguardia.",
+    "cvs_title": "CONTROLLO DI VERSIONE",
+    "cvs_description": "Utilizzo dei sistemi di controllo di versione come Git o SVN."
+  },
+  "works": {
+    "work1_title": "Pizzeria-Spicchi Di Sole",
+    "work1_description": "Disegno di Sito Web",
+    "work2_title": "Photographer Freelance",
+    "work2_description": "Portfolio Fotográfico",
+    "work3_title": "SantAngelo Frutta e Verdura",
+    "work3_description": "Disegno di Sito Web",
+    "work4_title": "Freelance Translator",
+    "work4_description": "Portfolio Personale",
+  },
+  "teams": {
+    "title": "Isaac Yriarte",
+    "subtitle": "Ingegnere Informatico",
+    "description": "Attualmente formo parte di una compagnia dove lavoro nello sviluppo di applicazioni web e mobili.
+<br/> Lavoro anche come libero professionista svilupando  progetti individualizzati.<br/>I linguaggi di programmazione più utilizzati da me sono: Java, C #, PHP, PHP OO, JavaScript, Python."
+  },
+  "Opinioni": {
+    "testimonial1": "\"Isaac es un desarrollador web talentoso y hábil, altamente comprometido con su trabajo, excelente profesional y con mucha calidad humana.\"",
+    "author1": "Aracelis Martín, Freelance RA Consultant",
+    "testimonial2": "\"Isaac is a highly responsible person, with a high academic and professional level, strongly recommended.\"",
+    "author2": "Giorgio Iule, Freelance Photographer.",
+    "testimonial3": "\"Isaac è un professionista preoccupato per il suo costante aggiornamento, orientato al raggiungimento degli obiettivi, dedicato, impegnato e appassionato del suo lavoro, altamente responsabile. Consegna i progetti nel tempo concordato.\"",
+    "author3": "Maurizio Buonpensiero, Commissionaria SantAngelo",
+    "testimonial4": "\"Recomiendo a Isaac para el trabajo individual, pero también para proyectos compartidos; por su disposición a colaborar con los miembros del equipo y su proactividad para contribuir con el logro de los objetivos.\"",
+    "author4": "Alejandra Yriarte, Corporación A&A"
+  },
+  "contact": {
+    "title": "Contattami",
+    "subtitle": "Chiedi il tuo preventivo gratuito. Sono disponibile per rispondere alle tue domande.",
+    "form": {
+      "placeholder": {
+        "name": "Nome",
+        "email": "email",
+        "message": "Il tuo Mesaggio"
+      },
+      "button_send": "INVIA"
+    }
+  },
+  "footer": {
+    "title": "Contatto",
+    //"credits": "Iconoos diseñados por <a href='http://www.freepik.com' title='Freepik'>Freepik</a> desde <a href='https://www.flaticon.es/' title='Flaticon'>www.flaticon.com</a> con licencia <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'"
+  },
+  "modal": {
+    "title": "Lingua",
+    "button_save": "Salvare"
+  }
+};
