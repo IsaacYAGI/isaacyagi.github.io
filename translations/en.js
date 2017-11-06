4b139a3a6e962834a5dfd1282b72fc2226baa99f
alelisa <alelisa@git.githubdemo>
2017-11-06 13:53:48 +0100

    Modifying english translation

--- translations/en.js
+++ translations/en.js
@@ -9,40 +9,41 @@ const i18nEn = {
   },
   "banner": {
     "title": "Isaac Yriarte",
-    "subtitle": "Programador Web/ App Developer"
+    "subtitle": "Web Programmer/ App Developer"
   },
   "intro": {
-    "title": "¡Puedo ayudarte con tu Proyecto!",
-    "description": "Soy una persona honesta, proactiva, responsable, comprometida y con disposición para la búsqueda continua de información a fin de ofrecer un servicio eficiente y soluciones de calidad a mis clientes.<br> Tengo disponibilidad para trabajar de manera presencial o remota."
+    "title": "I can help you with your Project!",
+    "description": "I am an honest, proactive, responsible person, always  committed with the continuous search of information in order to offer an efficient service and quality solutions to my clients.
+.<br> I am availabe to work in house or remotely."
   },
   "services": {
-    "consultAndAnalysis_title": "Consultoría & Análisis",
-    "consultAndAnalysis_description": "Análisis de los requerimientos  y necesidades del cliente en la creación de su proyecto.",
-    "designWeb_title": "Diseño y Desarrollo Web",
-    "designWeb_description": "Diseño y Desarrollo Web, utilizando lenguajes de programación y herramientas computacionales adecuadas.",
-    "designMobile_title": "Diseño y Desarrollo Web",
-    "designMobile_description": "Diseño y Desarrollo Web, utilizando lenguajes de programación y herramientas computacionales adecuadas.",
-    "control_title": "Control y Seguimiento",
-    "control_description": "Controles continuos del proceso y del producto, a fin de asegurar la satisfacción del cliente y el cumplimiento de los estándares de calidad.",
-    "maintenance_title": "Mantenimiento y Actualización de Páginas Web",
-    "maintenance_description": "Actualización de contenido, cambio de diseño y mantenimiento de páginas web ya existentes.",
-    "cvs_title": "Control de versiones",
-    "cvs_description": "Utilizando Sistemas de Control de Versiones como Git o SVN."
+    "consultAndAnalysis_title": "CONSULTANCY & ANALYSIS",
+    "consultAndAnalysis_description": "Analysis of the requirements and needs of the client in the creation of the project.",
+    "designWeb_title": "WEB DESIGN AND WEB  DEVELOPMENT",
+    "designWeb_description": "Web Design and Development, using programming languages and appropriate computational tools.",
+    "designMobile_title": "DESIGN AND DEVELOPMENT OF MOBILE APPLICATIONS.",
+    "designMobile_description": "Design and development of mobile applications for Android natively and multiplatform using Kony Studio.",
+    "control_title": "CONTROL AND FOLLOW UP",
+    "control_description": "Continuous process and product controls, in order to ensure customer satisfaction and compliance with quality standards.",
+    "maintenance_title": "MAINTAINING AND UPDATING OF WEB PAGES",
+    "maintenance_description": "Content update, design change, and maintenance of existing web pages.",
+    "cvs_title": "VERSION CONTROL",
+    "cvs_description": "Using Systems for  Version control  like Git or SVN."
   },
   "works": {
     "work1_title": "Pizzeria-Spicchi Di Sole",
-    "work1_description": "Diseño de Página Web",
+    "work1_description": "Web Page Design",
     "work2_title": "Photographer Freelance",
-    "work2_description": "Portafolio Fotográfico",
+    "work2_description": "Photo Portfolio",
     "work3_title": "SantAngelo Frutta e Verdura",
-    "work3_description": "Diseño de Página Web",
+    "work3_description": "Web Page Design",
     "work4_title": "Freelance Translator",
-    "work4_description": "Portafolio Personal",
+    "work4_description": "Personal Portfolio",
   },
   "teams": {
     "title": "Isaac Yriarte",
-    "subtitle": "Ingeniero en Informática",
-    "description": "Actualmente formo parte de una empresa donde me desempeño desarrollando aplicaciones web y móviles.<br/>  También desarrollo proyectos individuales como profesional freelance.<br/>Los lenguajes de programación que más utilizo actualmente son: Java, C#, PHP, PHP OO, JavaScript, Python."
+    "subtitle": "Computing Engineer",
+    "description": "Currently I am part of a company developing web and mobile applications.<br/> I also develop individual projects as a freelance professional.<br/>The programming languages I  commonly use are: Java, C #, PHP, PHP OO, JavaScript, Python."
   },
   "testimonials": {
     "testimonial1": "\"Isaac es un desarrollador web talentoso y hábil, altamente comprometido con su trabajo, excelente profesional y con mucha calidad humana.\"",
@@ -55,23 +56,23 @@ const i18nEn = {
     "author4": "Alejandra Yriarte, Corporación A&A"
   },
   "contact": {
-    "title": "Comunícate conmigo",
-    "subtitle": "Estoy a la orden para responder tus dudas y orientarte de manera gratuita sobre el proyecto que deseas realizar.",
+    "title": "CONTACT ME",
+    "subtitle": "Get a free quote. I am available to answer your queries about your project.",
     "form": {
       "placeholder": {
-        "name": "Nombre",
-        "email": "Correo",
-        "message": "Mensaje"
+        "name": "Name",
+        "email": "email",
+        "message": "Your message"
       },
-      "button_send": "Enviar"
+      "button_send": "SUBMIT"
     }
   },
   "footer": {
-    "title": "Contacto",
+    "title": "Contact",
     //"credits": "Iconoos diseñados por <a href='http://www.freepik.com' title='Freepik'>Freepik</a> desde <a href='https://www.flaticon.es/' title='Flaticon'>www.flaticon.com</a> con licencia <a href='http://creativecommons.org/licenses/by/3.0/' title='Creative Commons BY 3.0'"
   },
   "modal": {
-    "title": "Idioma",
-    "button_save": "Guardar"
+    "title": "Language",
+    "button_save": "Save"
   }
 };
